bac4f3016df688f3ea031026a069ea59e0969fde
Zhbert <zhbert@yandex.ru>
2020-11-30 15:02:30 +0300

    Command line arguments and setup mode

--- src/main/java/ru/zhbert/jscreenchanger/Main.java
+++ src/main/java/ru/zhbert/jscreenchanger/Main.java
@@ -13,31 +13,49 @@ public class Main {
 
     public static void main(String[] args) throws AWTException, IOException {
 
-        ArrayList<Screen> screenPool = new ArrayList<Screen>();
+        ArrayList<Screen> screenPool = new ArrayList<>();
 
+        HotKeyService hotKeyService = new HotKeyService(screenPool);
+
+        SettingsFileService settingsFileService = new SettingsFileService();
+        settingsFileService.readSettings();
+
+        viewScreenInfo(screenPool);
+
+        if (args.length > 0) {
+            String arg = args[0];
+            if (arg.equals("--setup")) {
+                hotKeyService.settingStart(settingsFileService);
+            } else {
+                hotKeyService.start();
+            }
+        } else {
+            hotKeyService.start();
+        }
+
+        getScanner();
+
+        System.out.println("Exiting program...");
+        hotKeyService.stop();
+    }
+
+    private static void getScanner() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a command: ");
+        int num = in.nextInt();
+        in.close();
+    }
+
+    private static void viewScreenInfo(ArrayList<Screen> screenPool) {
         GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] devices = environment.getScreenDevices();
         System.out.println("Screens found: " + devices.length);
-        for (int i=0; i<devices.length; i++) {
+        for (int i = 0; i < devices.length; i++) {
             DisplayMode displayMode = devices[i].getDisplayMode();
             screenPool.add(new Screen(displayMode.getWidth(), displayMode.getHeight(), i, devices[i]));
         }
         for (Screen screen : screenPool) {
             System.out.println(screen.toString());
         }
-
-        SettingsFileService settingsFileService = new SettingsFileService();
-        settingsFileService.readSettings();
-
-        HotKeyService hotKeyService = new HotKeyService(screenPool);
-        hotKeyService.settingStart(settingsFileService);
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a command: ");
-        int num = in.nextInt();
-        hotKeyService.stop();
-        System.out.printf("Your number: %d \n", num);
-        in.close();
-
     }
 }
